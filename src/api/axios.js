0d0a0df51488552fc0516d53a44dd1c75d373dda
Rodrigo Sumida <sumidarodrigo@gmail.com>
2025-11-26 11:19:23 -0300

    Link da API

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    // __prod__
-    baseURL: 'http://localhost:3002/api'
-})
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-export default api
+  // __prod__
+  // baseURL: 'http://localhost:3002/api',
+  baseURL: "https://modelcraft-api.onrender.com/api",
+});
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
+export default api;
